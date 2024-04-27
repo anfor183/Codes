e0564c4a9feb2e52661b6295d56be30979d47b7f
anfor183 <anukposif@gmail.com>
2024-04-27 19:37:06 +0100

    updated this directory

--- Ajax/first code project/index.js
+++
@@ -1,7 +0,0 @@
-let apiurlcomment = "https://jsonplaceholder.typicode.com/posts/1/comments";
-
-function apiresponse(response) {
-    console.log(response.data[0].body);
-}
-
-axios.get(apiurlcomment).then(apiresponse)
